332a67db84383e99ac59d2da1bfe5d0905a0be97
BadenHancock <hancockb24@hsnet.ahsd.org>
2023-03-19 23:47:58 -0400

    march 19, 2023 11:47pm

--- app/src/main/java/com/example/quiz/QuizViewModel.kt
+++ app/src/main/java/com/example/quiz/QuizViewModel.kt
@@ -3,40 +3,46 @@ package com.example.quiz
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 
-class QuizViewModel : Question()  {
-    val questions  = listOf<Question>(
-        Question(R.string.question_one, false, ),
+class QuizViewModel : Question() {
+    val questions = listOf<Question>(
+        Question(R.string.question_one, false,),
         Question(R.string.question_two, false),
         Question(R.string.question_three, true),
         Question(R.string.question_four, true),
-        Question(R.string.question_five, true))
+        Question(R.string.question_five, true)
+    )
 
     private val _incorrectAnswers = 0
-    val incorrectAnswers : Int
+    val incorrectAnswers: Int
         get() = _incorrectAnswers
 
     private val _correctAnswers = 0
-    val correctAnswers : Int
+    val correctAnswers: Int
         get() = _correctAnswers
 
     private val _listIndex = MutableLiveData(0)
-    val listIndex : LiveData<Int>
+    val listIndex: LiveData<Int>
         get() = _listIndex
 
 
     private val _gameWon = MutableLiveData(false)
-    val gameWon : LiveData<Boolean>
+    val gameWon: LiveData<Boolean>
         get() = _gameWon
 
-    val currentQuestionAnswer : Boolean
-        get() = questions.get(listIndex).isTrue
-    val currentQuestionText : String
-        get() = questions.get(listIndex).question.toString()
-    val currentQuestionCheatStatus : Boolean
-        get() = questions.get(listIndex).cheated
+    val currentQuestionAnswer: Boolean
+        get() = questions.get(_listIndex.toString().toInt()).isTrue
+    val currentQuestionText: String
+        get() = questions.get(_listIndex.toString().toInt()).question.toString()
+    val currentQuestionCheatStatus: Boolean
+        get() = questions.get(_listIndex.toString().toInt()).cheated
 
-    fun setCheatedStatusForCurrentQuestion( a : Boolean){
-        currentQuestionCheatStatus
+    fun setCheatedStatusForCurrentQuestion(a: Boolean) {
+        questions.get(_listIndex.toString().toInt()).cheated = a
     }
 
+    fun checkIfGameWon() {
+        if (_correctAnswers == 3) {
+            _gameWon.value = true
+        }
+    }
 }
